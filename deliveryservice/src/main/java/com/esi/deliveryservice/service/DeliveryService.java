aec007f6a77d9f20b4ff7a6a82edcfa14299d5a8
mammadli <gulnar.mammadli@ut.ee>
2023-06-05 16:24:33 +0300

    Task 3 add Kafka and listen to kitchen service,save it to db

--- deliveryservice/src/main/java/com/esi/deliveryservice/service/DeliveryService.java
+++ deliveryservice/src/main/java/com/esi/deliveryservice/service/DeliveryService.java
@@ -1,7 +1,10 @@
 package com.esi.deliveryservice.service;
 
 
+import com.esi.deliveryservice.dto.OrderDto;
+import com.esi.deliveryservice.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -14,11 +17,23 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class DeliveryService {
 
-@Autowired
-private DeliveryRepository deliveryRepository;
+    @Autowired
+    private DeliveryRepository deliveryRepository;
 
-@Autowired
-private WebClient.Builder webClientBuilder;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @KafkaListener(topics = "orderReadyTopic", groupId = "deliveryGroup")
+    private void getKitchenOrder(OrderDto orderDto) {
+        Order order = Order.builder()
+                .id(orderDto.getId())
+                .userId(orderDto.getUserId())
+                .pizzaCode(orderDto.getPizzaCode())
+                .pizzaQuantity(orderDto.getPizzaQuantity())
+                .orderStatus(orderDto.getOrderStatus())
+                .build();
+        deliveryRepository.save(order);
+    }
 
 
 }
