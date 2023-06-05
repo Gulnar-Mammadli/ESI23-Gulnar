a61e225dae1a51aef021851c0ccbcc629964a37d
mammadli <gulnar.mammadli@ut.ee>
2023-06-05 15:24:41 +0300

    Task 1 - addOrder endpoint updated with required parts

--- orderservice/src/main/java/com/esi/orderservice/service/OrderService.java
+++ orderservice/src/main/java/com/esi/orderservice/service/OrderService.java
@@ -1,21 +1,39 @@
 package com.esi.orderservice.service;
 
+import com.esi.orderservice.model.Order;
+import com.esi.orderservice.model.OrderStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.esi.orderservice.dto.OrderDto;
 import com.esi.orderservice.repository.OrderRepository;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+    private final KafkaTemplate<String, OrderDto> kafkaTemplate;
 
 
 
     public void addOrder(OrderDto orderDto) {
 
+        Order order = Order.builder()
+                .id(orderDto.getId())
+                .userId(orderDto.getUserId())
+                .pizzaCode(orderDto.getPizzaCode())
+                .pizzaQuantity(orderDto.getPizzaQuantity())
+                .address(orderDto.getAddress())
+                .build();
+        order.setOrderStatus(OrderStatus.Received);
+        orderDto.setOrderStatus(OrderStatus.Received);
+
+        orderRepository.save(order);
+        kafkaTemplate.send("orderCreatedTopic",orderDto);
     }
 
 }
